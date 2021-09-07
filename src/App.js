a86ddffee48d0b15fe788c5961f793db45f0d153
PavloVolkov <59417591+PavloVolkov@users.noreply.github.com>
2021-09-07 16:08:17 +0300

    qwert

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import Btn from './components/loadMoreBtn/button';
 function App() {
   return (
-    <div className="App">{<Btn />}</div>
+    // <div className="App">{<Btn />}</div>
+    <div className="App">
+      <div></div>
+      {/* <div>{<Btn />}</div> */}
+    </div>
   );
 }
 
